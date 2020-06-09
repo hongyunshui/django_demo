ab63542d7715e0385c693afe020278e3306be879
hongyunshui <jianhong513@163.com>
2020-06-09 12:32:00 +0800

    添加辖区数据及测试sqlite 使用

--- app01/tests.py
+++ app01/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+import sqlite3
+cn = sqlite3.connect('../db.sqlite3')
+cur = cn.execute('select * from "餐饮巡查台账"')
+
+print(cur.fetchmany(2))
