a2781de1ee516d1d2138725fcf4eb6c0213bf536
hongyunshui <jianhong513@163.com>
2020-09-13 23:30:29 +0800

    添加案情表和案卷来源码表

--- make_document/admin.py
+++ make_document/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import NaturalPerson
+from .models import NaturalPerson, RecordsSource, BaseRecordsTB
 
 # Register your models here.
 admin.site.register(NaturalPerson)
+admin.site.register(BaseRecordsTB)
+admin.site.register(RecordsSource)
+
 
